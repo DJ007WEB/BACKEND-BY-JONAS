d8eb1206078e613af8bef8895e423174e1d4b281
DJ007WEB <007officialjyo@gmail.com>
2024-07-09 09:00:13 +0530

    asynchronous code done

--- 01-NODE-FARM/index.js
+++ 01-NODE-FARM/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 // BLOCKING, SYNCHRONOUS WAY
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+/*const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 console.log(textIn);
 
@@ -10,6 +10,16 @@ const textOut = `This is what we know about avocado: ${textIn} \n Created on ${D
 
 fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("File has been written");
+console.log("File has been written"); */
 
 // NON BLOCKING ASYNCHRONOUS WAY
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
