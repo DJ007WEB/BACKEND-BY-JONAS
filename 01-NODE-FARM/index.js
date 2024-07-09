59b136c264cc7756d7342514765dee0158a844ea
DJ007WEB <007officialjyo@gmail.com>
2024-07-09 09:19:06 +0530

    created server

--- 01-NODE-FARM/index.js
+++ 01-NODE-FARM/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require('http');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// FILES
 
 // BLOCKING, SYNCHRONOUS WAY
 
@@ -14,12 +18,26 @@ console.log("File has been written"); */
 
 // NON BLOCKING ASYNCHRONOUS WAY
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written");
-      });
-    });
-  });
-});
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////// SERVER
+
+
+const server = http.createServer((req,res) => {
+    res.end('Hello from the Server');
+})
+
+
+server.listen(8000,'127.0.0.1',() => {
+    console.log('Listening to requests on port 8000');
+})
